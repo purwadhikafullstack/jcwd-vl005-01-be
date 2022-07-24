1861f4cbe8dffe131bb37d86d55358846b604f2f
dimgthb <98381997+dimgthb@users.noreply.github.com>
2022-07-24 09:29:04 +0700

    fix user -4

--- src/routers/userTransaction-routers.js
+++ src/routers/userTransaction-routers.js
@@ -2,6 +2,7 @@ const routers = require("express").Router();
 
 const { userTransaction_controller } = require('../controllers')
 
-routers.get('/user/historytransaction', userTransaction_controller.getHistoryTransaction)
+routers.get('/invoice/:userId', userTransaction_controller.getHistoryTransaction)
+routers.get('/user/test', userTransaction_controller.testPuppeter)
 
 module.exports = routers;
